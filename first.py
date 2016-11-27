ac7e8767641f081e0358c19a146f603b14ec64c2
wyattwhat <wyattsads@gmail.com>
2016-11-26 19:09:05 -0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,10 @@
+import random
+import sys
+import os
+age = 70
+if age > 18 :
+    print('ciggie 4 yo')
+elif age >= 69 :
+    print('yo 2 old 4 ciggie')
+else :
+    print('yo 2 old to be ciggin')
